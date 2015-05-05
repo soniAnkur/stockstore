dac1c5b8103f684dc5288bd758c052fe52235337
Danish <mohiuddun.ahamad@nagarro.com>
2015-05-05 16:07:12 +0530

    Commit Resource Factory

---
+++ stockstore/stockstore/app/factory/ResourceFactory.js
@@ -0,0 +1,8 @@
+var ss;
+(function (ss) {
+    var factory;
+    (function (factory) {
+        'use strict';
+    })(factory = ss.factory || (ss.factory = {}));
+})(ss || (ss = {}));
+//# sourceMappingURL=ResourceFactory.js.map
---
+++ stockstore/stockstore/app/factory/ResourceFactory.ts
@@ -0,0 +1,43 @@
+﻿module ss.factory {
+    'use strict';
+
+    //interface Istock {
+
+    //    errors: Array<string>;
+    //    id: string;
+    //    source_name: string;
+    //    source_code: string;
+    //    code: string;
+    //    name: string;
+    //    urlize_name: string;
+    //    display_url: URL;
+    //    description: string;
+    //    updated_at: Date;
+    //    frequency: number;
+    //    from_date: Date;
+    //    to_date: Date;
+    //    column_names: Array<string>;
+    //    premium: string;
+    //    data: Array<Array<string>>;
+    //}
+
+    //interface IfactoryResources {
+
+    //    getStockByProvider: (providerId : string) => Array<any>;
+
+    //}
+
+    //class Resourcesfactory implements IfactoryResources {
+
+    //    resourceService: ng.resource.IResourceService;
+    //    constructor($resource: ng.resource.IResourceService) {
+    //        this.resourceService = $resource;
+    //    }
+
+    //    getStockByProvider = (provider: string) => {
+    //        return this.resourceService("");
+    //    };
+    //}
+
+
+} 
